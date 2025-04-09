5107088ffd670e7884af0c2a95a7397a4f6a3da7
jtorrespe <jtorrespe@serversalafis.com>
2025-04-09 08:36:26 -0500

    se añade el bucle y el break

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -20,6 +20,8 @@ definimos cin para que el usuario pueda escribir
 cout muestra el texto a pantalla
 
 if (guessed_number = NUM) es un error, a guessed number no le estamos asignando igualdad, lo estamos haciendo 10, para comparar la igualdad usar ==, desigualdad !=, <, >
+
+else, si if no es cierto se procede al if, si if es verdadero se ignora todo el else
 */
 
 #include <iostream>
@@ -27,16 +29,19 @@ if (guessed_number = NUM) es un error, a guessed number no le estamos asignando 
 int main(void) {
     const int NUM = 10;
     int guessed_number;
-    std::cout <<"Adivina un número entre 1 y 100:\n";
-    std::cin >> guessed_number;
 
-    if (guessed_number == NUM){
-        std::cout << "Adivinaste\n";
-    } else if(guessed_number > NUM) {
-        std::cout << "Te pasaste\n";
-    } else if(guessed_number < NUM){
-        std::cout << "Te falta\n";
+    for( ; ; ){  
+        std::cout <<"Adivina un número entre 1 y 100:\n";
+        std::cin >> guessed_number;
+
+        if (guessed_number == NUM){
+            std::cout << "Adivinaste\n";
+            break;
+        } else if(guessed_number > NUM) {
+            std::cout << "Te pasaste\n";
+        } else if(guessed_number < NUM){
+            std::cout << "Te falta\n";
+        }
     }
-
     return 0;
 }
